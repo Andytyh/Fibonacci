8bb4fa46dcf17cb75dc259b44b5f112db756eaec
Andy Tong <at721@imperial.ac.uk>
2023-02-11 14:57:03 +0000

    style

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,4 +1,4 @@
-def fib(n) :
+def fib(n):
     """Return the n-th Fibonacci number"""
     if n == 0:
         return 0
@@ -6,4 +6,3 @@ def fib(n) :
         return 1
     else:
         return fib(n-2) + fib(n-1)
-        
